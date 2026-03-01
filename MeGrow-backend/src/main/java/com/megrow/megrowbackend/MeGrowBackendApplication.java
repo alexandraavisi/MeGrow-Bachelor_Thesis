cdd785544c17e2c3c3a360cf56d24edcd78789b4
Alexandra <avisinescu59@yahoo.com>
2026-03-01 12:09:22 +0200

    first commit

---
+++ MeGrow-backend/src/main/java/com/megrow/megrowbackend/MeGrowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.megrow.megrowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeGrowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeGrowBackendApplication.class, args);
+    }
+
+}
---
+++ MeGrow-backend/src/test/java/com/megrow/megrowbackend/MeGrowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megrow.megrowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeGrowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

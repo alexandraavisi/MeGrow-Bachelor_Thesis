86c7ee178b87754257a675775479e7f8603f2e20
Alexandra <avisinescu59@yahoo.com>
2026-03-03 12:28:37 +0200

    onboarding entities commit

---
+++ MeGrow-backend/src/main/java/com/megrow/megrowbackend/entity/OnboardingAnswer.java
@@ -0,0 +1,34 @@
+package com.megrow.megrowbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "onboarding_answers",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "question_index"})
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OnboardingAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",  nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_index",  nullable = false)
+    private OnboardingQuestion question;
+
+    @Column(name = "answer_value",  nullable = false)
+    private short answerValue;
+}
---
+++ MeGrow-backend/src/main/java/com/megrow/megrowbackend/entity/OnboardingQuestion.java
@@ -0,0 +1,27 @@
+package com.megrow.megrowbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "onboarding_questions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OnboardingQuestion {
+    @Id
+    @Column(name = "question_index",  nullable = false)
+    private short questionIndex;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String text;
+
+    @Column(nullable = false, length = 1)
+    private String dimension;
+
+    @Column(name = "is_reverse", nullable = false)
+    private boolean isReverse;
+}

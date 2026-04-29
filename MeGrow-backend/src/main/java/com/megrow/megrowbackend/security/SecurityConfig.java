8a46a5a5fde3bb75a71dd12d9441d35ebf6325e5
Alexandra <avisinescu59@yahoo.com>
2026-04-29 12:40:18 +0300

    Swagger OpenApi documentation commit

--- MeGrow-backend/src/main/java/com/megrow/megrowbackend/security/SecurityConfig.java
+++ MeGrow-backend/src/main/java/com/megrow/megrowbackend/security/SecurityConfig.java
@@ -28,9 +28,9 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
     }
 
